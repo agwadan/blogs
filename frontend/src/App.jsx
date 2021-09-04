600f36d53ff63766bceb495dca112085153e8b84
agwadan <agwa.dan@gmail.com>
2021-09-04 19:28:43 +0300

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Blogs Microservices</h1>
+    </div>
+  )
+}
+
+export default App
