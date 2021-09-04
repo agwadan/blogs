d7df614605a17f66298425be5cf0bd54f2e52f97
agwadan <agwa.dan@gmail.com>
2021-09-04 19:39:57 +0300

    App get and post routes for posts

--- posts/index.js
+++ posts/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const { randomBytes } = require('crypto');
 const app = express();
 
-app.get('/posts', (req, res) => {
+app.use(express.json());
 
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
 });
 
 app.post('/posts', (req, res) => {
-
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  posts[id] = {
+    id, title
+  };
+  res.status(201).send(posts[id]); //res "201" for a new resource created
 });
 
 app.listen(4000, () => { console.log(`Posts listening at port 4000`) });
