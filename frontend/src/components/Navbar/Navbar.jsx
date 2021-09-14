810d707b72b12f72a62e999b1a6930e31a4f9bb4
agwadan <agwa.dan@gmail.com>
2021-09-14 23:16:00 +0300

    Add spacer to navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
       <nav className='navbar-nav'>
         <div></div>
         <div className='navbar-logo'><a href="/">BLOGS</a></div>
+        <div className='spacer' />
         <div className='navbar-items'>
           <ul>
             <li><a href='/'>Posts</a></li>
