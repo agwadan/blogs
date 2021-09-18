5d93983874ea0fe19254a881d3b175eb54e83def
agwadan <agwa.dan@gmail.com>
2021-09-18 23:59:25 +0300

    Create and style side drawer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BackDrop } from './components/BackDrop/BackDrop';
 import { CreatePost } from './components/CreatePost';
 import Navbar from './components/Navbar/Navbar';
 import { PostList } from './components/PostList/PostList';
+import { SideDrawer } from './components/SideDrawer/SideDrawer';
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="App" style={{ height: '100% ' }}>
       <Navbar />
+      <SideDrawer />
+      <BackDrop />
       <CreatePost />
       <hr />
       <PostList />
---
+++ frontend/src/components/SideDrawer/SideDrawer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SideDrawer.css';
+
+export const SideDrawer = (props) => {
+  return (
+    <nav className='side-drawer'>
+      <ul>
+        <li>Posts</li>
+        <li>Comments</li>
+      </ul>
+    </nav>
+  )
+}
